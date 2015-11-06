6af9cc7f2f07991854b71717650d94aa171a6554
dnlnegroni <dnl.negroni@gmail.com>
2015-11-06 19:28:27 +0100

    QRWebPageActivity

---
+++ src/com/ogc/action/edit/Qrwebpage.java
@@ -0,0 +1,35 @@
+package com.ogc.action.edit;
+
+import android.content.Context;
+
+import com.example.qrboard.ARGUI;
+import com.ogc.action.Action;
+import com.ogc.model.QRSquare;
+import com.ogc.model.QRUser;
+
+public class Qrwebpage extends Action{
+	
+	private ARGUI argui;
+	private QRSquare qrSquare;
+	private QRUser user;
+	private Context context;
+	
+	@Override
+	public int getColor(ARGUI argui) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void prepare(ARGUI argui) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addQRParameter(QRSquare qrsquare) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
